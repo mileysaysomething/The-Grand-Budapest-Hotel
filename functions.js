e57da63cb15ea36dcb6550c2cf3f3547fbe4cf85
vii.mileyc <chase_was_here919@hotmail.com>
2019-10-23 18:19:41 -0400

    Created 'Cast Grid'
    - Shows the grid when users scrolled 1/3 of the screen from top
    - Three Cast per row
    - Shows one by one

--- functions.js
+++ functions.js
@@ -1,15 +1,38 @@
-$(window).scroll(function(){
+
+
+
+$(window).scroll(function () {
 
     var scroll = $(this).scrollTop();
+
+    /* ------------------------------ Banner ---------------------------- */
 
     //Mid layer
     //When scroll the Banner Title moves with 0.5 speed, based on distance scrolled.
     $('.banner-title')
-    .css({'transform':'translate(0px,' + scroll / 2 + '%)'
-    });
+        .css({
+            'transform': 'translate(0px,' + scroll / 2 + '%)'
+        });
 
     //Front layer
     $('.banner-cast')
-    .css({'transform':'translate(0px, -' + scroll / 5 + '%)'
-    });
+        .css({
+            'transform': 'translate(0px, -' + scroll / 5 + '%)'
+        });
+
+    /* ------------------------------ Cast Grid ---------------------------- */
+
+    // How far scrolled from the top > container from the top - one third of the screen
+    if (scroll > $('.container').offset().top - ($(window).height()/1.3) ){
+
+        $('.container div').each(function (i) {
+
+            //Shows one by one
+            setTimeout(function () {
+                $('.container div').eq(i).addClass('isShowing'); // Add isShowing
+            }, 150 * (i + 1));
+
+
+        });
+    }
 });
